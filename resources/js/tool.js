45f82b9c4e116b5a22c14cc43b5bcc8cb4db6927
yaserzizo <yasser.elbasiouny@gmail.com>
2020-01-06 07:12:54 +0200

    Update tool.js

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,9 +1,18 @@
+import VueTooltip from 'v-tooltip';
 Nova.booting((Vue, router) => {
+
     router.addRoutes([
         {
             name: 'nova-filemanager',
             path: '/nova-filemanager',
-            component: require('./components/Tool'),
+            component: require('./components/Tool').default,
         },
     ]);
+
+
+
+    Vue.use(VueTooltip);
+    VueTooltip.options.defaultTemplate = '<div dir="rtl" class="tooltip-vue" role="tooltip"><div class="tooltip-vue-arrow"></div><div class="tooltip-vue-inner"></div></div>';
+    VueTooltip.options.defaultArrowSelector = '.tooltip-vue-arrow, .tooltip-vue__arrow';
+    VueTooltip.options.defaultInnerSelector = '.tooltip-vue-inner, .tooltip-vue__inner';
 });
